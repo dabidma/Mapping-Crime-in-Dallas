4be1afa99d976fd0571e075e201ecfef44927f5f
unknown <dvdmasnk@gmail.com>
2022-10-31 20:43:52 -0500

    first tests

---
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,4 @@
+keyid = 'jauekgsct7nsjj0oaf2t1yv5'
+keysecret = '5t6w7xdfn0lhbvq1y34x11j2dd8n107nezo09q54t9db7647b2'
+app_token = 'uy5mAX4bY07AjFhGfDLAOBrs5'
+token_secret = 'JYufA8bg58ARLOv7u6oEtcMENCQiCMYs_13Q'
