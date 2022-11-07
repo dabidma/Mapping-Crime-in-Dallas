8d5a189fc03791ab86d004af1a7f0fc2b1077620
unknown <dvdmasnk@gmail.com>
2022-11-07 15:28:26 -0600

    modified data pull

--- app.py
+++ app.py
@@ -2,13 +2,23 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import api_data
 import data_json
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def echo():
-    pull = data_json.json_data()
-    return render_template('index.html', test = pull)
+    #remove files if already exists to upload new data
+    try:
+        os.remove('js\month_data.js')
+        os.remove('js/today_data.js')
+        os.remove('js\week_data.js')
+        os.remove('js\year_data.js')
+    except:
+        print('no files to be removed')
+    #call function to get all the necessary data
+    data_json.json_data()
+    return render_template('index.html')
 
 
 #debugger to edit while running
