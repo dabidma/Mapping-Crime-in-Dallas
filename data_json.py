77c176df2ded31fe7edd066c5c7b298d3b9bd665
unknown <dvdmasnk@gmail.com>
2022-11-09 04:25:04 -0600

    fixed data incorrectly loading from api pull tweaks

--- data_json.py
+++ data_json.py
@@ -21,19 +21,19 @@ def json_data():
 
     for row in rows:
         d = collections.OrderedDict()
-        d['incidentnum'] = row[0]
-        d['arrestnumber'] = row[1]
-        d['ararrestdate'] = row[2]
-        d['ararresttime'] = row[3]
-        d['arpremises'] = row[4]
-        d['arladdress'] = row[5]
-        d['arlzip'] = row[6]
-        d['sex'] = row[7]
-        d['drugrelated'] = row[8]
-        d['drugtype'] = row[9]
-        d['age'] = row[10]
-        d['lat'] = row[11]
-        d['lon'] = row[12]
+        d['incidentnum'] = row[1]
+        d['arrestnumber'] = row[2]
+        d['ararrestdate'] = row[4]
+        d['ararresttime'] = row[5]
+        d['arpremises'] = row[5]
+        d['arladdress'] = row[6]
+        d['arlzip'] = row[7]
+        d['sex'] = row[8]
+        d['drugrelated'] = row[9]
+        d['drugtype'] = row[10]
+        d['age'] = row[11]
+        d['lat'] = row[12]
+        d['lon'] = row[-1]
         data_list.append(d)
         j = json.dumps(data_list)
 
@@ -49,20 +49,20 @@ def json_data():
 
     for row in rows:
         a = collections.OrderedDict()
-        a['incidentnum'] = row[0]
-        a['arrestnumber'] = row[1]
-        a['ararrestdate'] = row[2]
-        a['ararresttime'] = row[3]
-        a['arpremises'] = row[4]
-        a['arladdress'] = row[5]
-        a['arlzip'] = row[6]
-        a['sex'] = row[7]
-        a['drugrelated'] = row[8]
-        a['drugtype'] = row[9]
-        a['age'] = row[10]
-        a['lat'] = row[11]
-        a['lon'] = row[12]
-        month_list.append(a)
+        a['incidentnum'] = row[1]
+        a['arrestnumber'] = row[2]
+        a['ararrestdate'] = row[4]
+        a['ararresttime'] = row[5]
+        a['arpremises'] = row[5]
+        a['arladdress'] = row[6]
+        a['arlzip'] = row[7]
+        a['sex'] = row[8]
+        a['drugrelated'] = row[9]
+        a['drugtype'] = row[10]
+        a['age'] = row[11]
+        a['lat'] = row[12]
+        a['lon'] = row[-1]
+        data_list.append(a)
         x = json.dumps(month_list)
 
     with open('static\js\month_data.js', 'w') as f:
@@ -77,19 +77,19 @@ def json_data():
 
     for row in rows:
         b = collections.OrderedDict()
-        b['incidentnum'] = row[0]
-        b['arrestnumber'] = row[1]
-        b['ararrestdate'] = row[2]
-        b['ararresttime'] = row[3]
-        b['arpremises'] = row[4]
-        b['arladdress'] = row[5]
-        b['arlzip'] = row[6]
-        b['sex'] = row[7]
-        b['drugrelated'] = row[8]
-        b['drugtype'] = row[9]
-        b['age'] = row[10]
-        b['lat'] = row[11]
-        b['lon'] = row[12]
+        b['incidentnum'] = row[1]
+        b['arrestnumber'] = row[2]
+        b['ararrestdate'] = row[4]
+        b['ararresttime'] = row[5]
+        b['arpremises'] = row[5]
+        b['arladdress'] = row[6]
+        b['arlzip'] = row[7]
+        b['sex'] = row[8]
+        b['drugrelated'] = row[9]
+        b['drugtype'] = row[10]
+        b['age'] = row[11]
+        b['lat'] = row[12]
+        b['lon'] = row[-1]
         data_list.append(b)
         week_dump = json.dumps(data_list)
 
@@ -105,19 +105,19 @@ def json_data():
 
     for row in rows:
         c = collections.OrderedDict()
-        c['incidentnum'] = row[0]
-        c['arrestnumber'] = row[1]
-        c['ararrestdate'] = row[2]
-        c['ararresttime'] = row[3]
-        c['arpremises'] = row[4]
-        c['arladdress'] = row[5]
-        c['arlzip'] = row[6]
-        c['sex'] = row[7]
-        c['drugrelated'] = row[8]
-        c['drugtype'] = row[9]
-        c['age'] = row[10]
-        c['lat'] = row[11]
-        c['lon'] = row[12]
+        c['incidentnum'] = row[1]
+        c['arrestnumber'] = row[2]
+        c['ararrestdate'] = row[4]
+        c['ararresttime'] = row[5]
+        c['arpremises'] = row[5]
+        c['arladdress'] = row[6]
+        c['arlzip'] = row[7]
+        c['sex'] = row[8]
+        c['drugrelated'] = row[9]
+        c['drugtype'] = row[10]
+        c['age'] = row[11]
+        c['lat'] = row[12]
+        c['lon'] = row[-1]
         data_list.append(c)
         today_json = json.dumps(data_list)
     
